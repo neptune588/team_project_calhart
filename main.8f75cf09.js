ec75c9c5fbb295ed33c70b37d4e3e1ecf1ca528f
seohwan_YUN <ctr_nike@naver.com>
2023-06-14 10:51:31 +0900

    Updates

--- main.8f75cf09.js
+++ main.8f75cf09.js
@@ -270,6 +270,7 @@ function Module(moduleName) {
 module.bundle.Module = Module;
 var checkedAssets, assetsToAccept;
 var parent = module.bundle.parent;
+
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
